c56ac61c681413ddb699c8dc56218966820e3a79
Ivan Skytte Jørgensen <isj-github@privacore.com>
2017-06-02 12:17:39 +0200

    webserver: provide details

--- vagus_webserver.py
+++ vagus_webserver.py
@@ -50,6 +50,25 @@ def get_instance_list(cluster_id):
 	#do we want to split/parse into instance-id + extra_information? Nah...
 	return r.split('\n')[0:-2]
 
+def get_instance_listx(cluster_id):
+	r = do_command("pollx %s"%cluster_id)
+	if not r or len(r)==0:
+		return None
+	if r=="\n":
+		return []
+	#do we want to split/parse into instance-id + extra_information? Nah...
+	l = []
+	for line in r.split('\n')[0:-2]:
+		instance_id = line.split(':')[0]
+		vagus_id = line.split(':')[1]
+		lifetime = float(line.split(':')[2])
+		if len(line.split(':'))>3:
+			extra_info = line.partition(':')[2].partition(':')[2].partition(':')[2]
+		else:
+			extra_info = ""
+		l.append( (vagus_id,instance_id,lifetime,extra_info) )
+	return l
+
 def get_vagus_list():
 	r = do_command("getvaguslist")
 	if not r or len(r)==0:
@@ -86,6 +105,8 @@ class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
 		s = parsed_url.path.split("/")
 		if len(s)==3 and s[1]=="cluster":
 			return self.serve_cluster(s[2])
+		if len(s)==3 and s[1]=="cluster_details":
+			return self.serve_cluster_details(s[2])
 		self.send_response(404)
 		self.end_headers()
 	
@@ -122,7 +143,7 @@ class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
 		print >>self.wfile, '<h1>Known clusters</h1>'
 		print >>self.wfile, '<ul>'
 		for cluster in cluster_list:
-			print >>self.wfile, '<li><a href="/cluster/%s">%s</a></li>'%(cluster,cluster)
+			print >>self.wfile, '<li><a href="/cluster/%s">%s</a> (<a href="/cluster_details/%s">details</a>)'%(cluster,cluster,cluster)
 		print >>self.wfile, '</ul>'
 			
 		print >>self.wfile, '<h1>Known Vagus processes</h1>'
@@ -177,6 +198,70 @@ class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
 			print >>self.wfile, '<li>%s</li>'%(instance)
 		print >>self.wfile, '</ul>'
 			
+		print >>self.wfile, '</body>'
+		print >>self.wfile, '</html>'
+	
+	def serve_cluster_details(self,cluster_id):
+		instance_list = get_instance_listx(cluster_id)
+		if instance_list==None:
+			return self.serve_vaugs_talk_error("Could not get instance list from vagus")
+		#we want the instances to be sorted but we have to check if they should be numeriacally or alphamerically sorted
+		all_numeric = True
+		for i in instance_list:
+			if not i[1][0].isdigit():
+				all_numeric = False
+				break
+		if all_numeric:
+			def cmp_numeric(a,b):
+				x = cmp(a[0],b[0])
+				if x!=0:
+					return x
+				else:
+					return cmp(int(a[1]),int(b[1]))
+			instance_list.sort(cmp=cmp_numeric)
+		else:
+			def cmp_alfameric(a,b):
+				x = cmp(a[0],b[0])
+				if x!=0:
+					return x
+				else:
+					return cmp(a[1],b[1])
+			instance_list.sort(cmp=cmp_alfameric)
+		
+		#count the number of instances in each vagus-instance
+		instance_count = {}
+		for instance in instance_list:
+			if instance[0] not in instance_count:
+				instance_count[instance[0]] = 0
+			instance_count[instance[0]] += 1
+		
+		self.send_response(200)
+		self.send_header("Content-type", "text/html; charset=utf-8")
+		self.send_header("cache-control","max-age=0")
+		self.end_headers()
+		print >>self.wfile, '<html>'
+		print >>self.wfile, '<head>'
+		print >>self.wfile, '<title>Vagus: instances in %s</title>'%(cluster_id)
+		print >>self.wfile, '<meta name="viewport" content="width=device-width, initial-scale=1.0"/>'
+		#print >>self.wfile, '<link rel="stylesheet" type="text/css" href="default.css" title="Default"/>'
+		print >>self.wfile, '</head>'
+		print >>self.wfile, '<body>'
+		
+		print >>self.wfile, '<h1>Alive instances (%d)</h1>'%(len(instance_list))
+		print >>self.wfile, '<ul>'
+		previous_vagus_instance_id = None
+		for instance in instance_list:
+			if instance[0]!=previous_vagus_instance_id:
+				if previous_vagus_instance_id!=None:
+					print >>self.wfile, '    </ul></li>'
+				print >>self.wfile, '<li>%s (%d instances)'%(instance[0], instance_count[instance[0]])
+				print >>self.wfile, '    <ul>'
+				previous_vagus_instance_id = instance[0]
+			print >>self.wfile, '    <li>%s:%s:%s:%s</li>'%(instance[0],instance[1],instance[2],instance[3])
+		if previous_vagus_instance_id!=None:
+			print >>self.wfile, '    </ul></li>'
+		print >>self.wfile, '</ul>'
+		
 		print >>self.wfile, '</body>'
 		print >>self.wfile, '</html>'
 	
